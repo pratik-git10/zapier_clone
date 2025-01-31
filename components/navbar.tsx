cdf8b8b0128830622bd32436f6470d4bc557fffa
Pratik <93414233+pratik-git10@users.noreply.github.com>
2025-01-31 23:52:51 +0530

    Updated Navbar Page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,27 @@
 import Link from "next/link";
 import React from "react";
 
+const navLinks = [
+  { link: "/", title: "Home" },
+  { link: "/about", title: "About" },
+  { link: "/contact", title: "Contact" },
+];
 const Navbar = () => {
   return (
-    <div className="flex justify-between py-2 px-4 bg-gray-400 mx-2 rounded-b-md">
-      <Link href="/">Zapier</Link>
+    <div className="flex justify-between py-2 px-4 bg-gray-400 mx-2 rounded-b-md items-center">
+      <Link href="/" className="font-bold text-xl text-slate-800">
+        Zapier
+      </Link>
+
       <div className="flex gap-8">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
+        {navLinks.map((links, index) => (
+          <Link
+            key={index}
+            href={links.link}
+            className="hover:bg-red-100 hover:transition ease-in-out p-1 rounded-md hover:scale-105 hover:shadow-md">
+            {links.title}
+          </Link>
+        ))}
       </div>
     </div>
   );
